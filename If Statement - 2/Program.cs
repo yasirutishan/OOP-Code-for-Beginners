282ac9ff355b426991fb89336866def4f11058b2
yasirutishan <85503750+yasirutishan@users.noreply.github.com>
2021-08-23 11:55:34 +0530

    Add files via upload

---
+++ If Statement - 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace If_Statement___2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            if (a == b)// if the condition ture then run the block
+            {
+                Console.WriteLine("a equal b");
+            }
+            else if (a > b) 
+            {
+                Console.WriteLine("a greater than b");
+            }
+            else
+            {
+                Console.WriteLine("none");
+            }
+        }
+    }
+}
---
+++ Logical Operators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Logical_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine(a > b && a >= b);//false && false => false
+            Console.WriteLine(a < b && a >= b);//ture && false => false
+            Console.WriteLine(a > b && a <= b);//false && ture => fales
+            Console.WriteLine(a < b && a != b);//ture && ture => ture
+
+            Console.WriteLine(a > b || a >= b);//false && false => fales
+            Console.WriteLine(a < b || a >= b);//ture && false => ture
+            Console.WriteLine(a > b || a <= b);//false && ture => ture
+            Console.WriteLine(a < b || a != b);//ture && ture => ture
+
+            Console.WriteLine(a > b);//false 
+            Console.WriteLine(a < b);//ture 
+
+            Console.WriteLine(!(a > b ));//false =>ture
+            Console.WriteLine(!(a < b));//ture =>false
+
+
+        }
+    }
+}
