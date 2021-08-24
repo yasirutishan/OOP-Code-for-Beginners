23002831a9ef1b8a10b66240ea662aadd9db410d
yasirutishan <85503750+yasirutishan@users.noreply.github.com>
2021-08-24 11:00:27 +0530

    Add files via upload

---
+++ While Loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+
+            while (a<5)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+        }
+    }
+}
